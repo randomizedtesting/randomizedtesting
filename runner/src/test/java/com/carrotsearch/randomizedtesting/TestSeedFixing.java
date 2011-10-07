1c3176f42ffe752be7fbad2c22e21a3a336fd881
Dawid Weiss <dawid.weiss@carrotsearch.com>
2011-10-07 09:56:18 +0200

    Added a runner @Seed fixing test.

--- runner/src/test/java/com/carrotsearch/randomizedtesting/TestSeedFixing.java
+++ runner/src/test/java/com/carrotsearch/randomizedtesting/TestSeedFixing.java
@@ -1,14 +1,15 @@
 package com.carrotsearch.randomizedtesting;
 
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 import org.junit.runner.RunWith;
-
-import com.carrotsearch.randomizedtesting.RandomizedContext;
-import com.carrotsearch.randomizedtesting.RandomizedRunner;
-import com.carrotsearch.randomizedtesting.Seed;
-
-import static org.junit.Assert.*;
 
 /**
  * Seed fixing for static fixtures and/or methods using annotations.
@@ -25,5 +26,43 @@ public class TestSeedFixing {
   @Test
   public void dummy() {
     assertEquals(0xcafebabeL, RandomizedContext.current().getRandomness().getSeed());
+  }
+
+  @Seed("deadbeef")
+  @RunWith(RandomizedRunner.class)
+  public static class ExpectSeed {
+    static Map<String, Object> seeds = new HashMap<String, Object>();
+
+    @Test
+    public void testMethod1() {
+      checkSeed("method1");
+    }
+
+    @Test
+    public void testMethod2() {
+      checkSeed("method2");
+    }
+    
+    private void checkSeed(String key) {
+      final Object seed = RandomizedContext.current().getRandomness().getSeed();
+      if (seeds.containsKey(key)) {
+        assertEquals(seeds.get(key), seed);
+      } else {
+        seeds.put(key, seed);
+      }
+    }    
+  }
+  
+  /**
+   * Check if methods get the same seed on every run with a fixed runner's seed.
+   */
+  @Test
+  public void testSameMethodRandomnessWithFixedRunner() {
+    Result result = JUnitCore.runClasses(ExpectSeed.class);
+    assertTrue(result.getFailures().toString(), result.wasSuccessful());
+    result = JUnitCore.runClasses(ExpectSeed.class);
+    assertTrue(result.wasSuccessful());
+    result = JUnitCore.runClasses(ExpectSeed.class);    
+    assertTrue(result.wasSuccessful());
   }
 }
