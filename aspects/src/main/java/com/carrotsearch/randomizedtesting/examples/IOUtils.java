c5f059a17f13b4cd16e21dc6129a6d3437d7321e
Dawid Weiss <dawid.weiss@carrot-search.com>
2011-10-16 00:36:32 +0200

    Code formatting.

--- aspects/src/main/java/com/carrotsearch/randomizedtesting/examples/IOUtils.java
+++ aspects/src/main/java/com/carrotsearch/randomizedtesting/examples/IOUtils.java
@@ -12,23 +12,22 @@ import com.carrotsearch.randomizedtesting.aspects.TrackTempDirLocks;
 
 /**
  * This is an example of a "business class" that we will test using
- * {@link RandomizedRunner} and aspect to simulate real-life problems
- * ({@link DiskProblems}) or code issues ({@link TrackTempDirLocks}).
+ * {@link RandomizedRunner} and aspect to simulate real-life problems (
+ * {@link DiskProblems}) or code issues ({@link TrackTempDirLocks}).
  */
-public class IOUtils
-{
-    /**
-     * Read a file from disk and convert it to a Java String using the given
-     * charset. 
-     */
-    public static String readFile(File file, Charset charset) throws IOException {
-        byte [] bytes = new byte [(int) file.length()];
-        DataInputStream is = new DataInputStream(new FileInputStream(file));
-        try {
-            is.readFully(bytes);
-        } finally {
-            is.close(); // don't suppress IOException if it happens.
-        }
-        return new String(bytes, charset);
+public class IOUtils {
+  /**
+   * Read a file from disk and convert it to a Java String using the given
+   * charset.
+   */
+  public static String readFile(File file, Charset charset) throws IOException {
+    byte[] bytes = new byte[(int) file.length()];
+    DataInputStream is = new DataInputStream(new FileInputStream(file));
+    try {
+      is.readFully(bytes);
+    } finally {
+      is.close(); // don't suppress IOException if it happens.
     }
+    return new String(bytes, charset);
+  }
 }
--- aspects/src/test/java/com/carrotsearch/randomizedtesting/examples/TestIOUtils.java
+++ aspects/src/test/java/com/carrotsearch/randomizedtesting/examples/TestIOUtils.java
@@ -10,25 +10,28 @@ import org.junit.Test;
 import com.carrotsearch.randomizedtesting.RandomizedTest;
 import com.carrotsearch.randomizedtesting.annotations.Repeat;
 
-public class TestIOUtils extends RandomizedTest
-{
-    @Test @Repeat(iterations = 10)
-    public void testReadFile() throws IOException {
-        File tempFile = newTempFile();
-        FileOutputStream fos = new FileOutputStream(tempFile);
-        fos.write("Hello Barcelona".getBytes(UTF8));
-        fos.close();
-
-        String readBack = IOUtils.readFile(tempFile, UTF8);
-        Assert.assertEquals("Hello Barcelona", readBack);
-    }
+public class TestIOUtils extends RandomizedTest {
+  @Test
+  @Repeat(iterations = 10)
+  public void testReadFile() throws IOException {
+    File tempFile = newTempFile();
+    FileOutputStream fos = new FileOutputStream(tempFile);
+    String contents = randomUnicodeString();
+    fos.write(contents.getBytes(UTF8));
+    fos.close();
     
-    @Test
-    public void testLockFolder() throws IOException {
-        File tempDir = newTempDir();
-        FileOutputStream fos = new FileOutputStream(new File(tempDir, "testFile.txt"));
-        fos.write("Hello Barcelona".getBytes(UTF8));
-        // Don't close fos - this results in the parent folder locked (at least on Windows).
-        // fos.close();
-    }
+    String readBack = IOUtils.readFile(tempFile, UTF8);
+    Assert.assertEquals(contents, readBack);
+  }
+
+  @Test
+  public void testLockFolder() throws IOException {
+    File tempDir = newTempDir();
+    FileOutputStream fos = new FileOutputStream(new File(tempDir,
+        "testFile.txt"));
+    fos.write("Hello Barcelona".getBytes(UTF8));
+    // Don't close fos - this results in the parent folder locked (at least on
+    // Windows).
+    // fos.close();
+  }
 }
