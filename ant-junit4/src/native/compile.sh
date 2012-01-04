0367ec2175a33f3aa4243d4643f40bcb04719095
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-01-04 14:17:06 +0100

    Windows64 bit crash recompiled under cygwin and working. Win32 does not work, surprisingly.

--- ant-junit4/src/native/compile.sh
+++ ant-junit4/src/native/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CFLAGS="-I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/linux"
+CFLAGS="-I ${JAVA_HOME}/include -I ${JAVA_HOME}/include/linux -I ${JAVA_HOME}/include/win32"
 
 # Linux, 64-bit.
 gcc -m64 ${CFLAGS} -fPIC -c crash.c
@@ -11,13 +11,11 @@ gcc -m32 ${CFLAGS} -fPIC -c crash.c
 ld -melf_i386 -shared -static -o lib/libcrash.so *.o
 
 # Windows, 32-bit
-rm *.o
-i686-w64-mingw32-gcc ${CFLAGS} -fPIC -c crash.c
-i686-w64-mingw32-ld -shared -o lib/crash.dll *.o
+rm -f *.o
+i686-w64-mingw32-gcc ${CFLAGS} -shared -o lib/crash.dll crash.c
 
 # Windows, 64-bit
-rm *.o
-x86_64-w64-mingw32-gcc ${CFLAGS} -fPIC -c crash.c
-x86_64-w64-mingw32-ld -shared -o lib/crash64.dll *.o
+rm -f *.o
+x86_64-w64-mingw32-gcc ${CFLAGS} -shared -o lib/crash64.dll crash.c 
 
 rm *.o
