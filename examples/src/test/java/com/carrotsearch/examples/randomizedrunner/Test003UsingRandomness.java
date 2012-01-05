6847de81a89cb9b189bd91b445e9f223cb9f3004
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-01-05 23:26:51 +0100

    GH-42: more examples.

--- examples/src/test/java/com/carrotsearch/examples/randomizedrunner/Test003UsingRandomness.java
+++ examples/src/test/java/com/carrotsearch/examples/randomizedrunner/Test003UsingRandomness.java
@@ -19,7 +19,7 @@ import com.carrotsearch.randomizedtesting.*;
  * </ul>
  * 
  * <p>
- * Let's see this on a simple example. Let's say we have a method that adds two
+ * Let's see this on a simple example of a method that adds two
  * integers ({@link Adder#add(int, int)}). We can test this method using a "fixed" test
  * case as shown in {@link #fixedTesting} but this test will always execute in
  * an identical way (which is good if you're looking for regression coverage but
@@ -29,9 +29,17 @@ import com.carrotsearch.randomizedtesting.*;
  * A randomized test, on the other hand, will pick parameters from a larger
  * spectrum of values and assert on the method's contract. Here, we can make
  * sure the sum is always larger or equal than the arguments given two positive
- * integers. This assertion will fail quite often because of overflows as shown
+ * arguments. This assertion will fail quite often because of integer overflows as shown
  * in {@link #randomizedTesting()} (re-run the test a few times if it doesn't
  * fail the first time).
+ * 
+ * <p>
+ * While the above example is trivial most of the bugs in code stem from similar
+ * subtleties (possibly resulting from more complex interactions). In many cases
+ * the "contract" that we can assert on can be stated as "throw no-exception" given
+ * valid arguments. An example of that is in method {@link #expectNoException()}.
+ * At first glance this method will work most of the time, but try passing
+ * {@link Integer#MIN_VALUE} as a random number and see what will happen.
  */
 public class Test003UsingRandomness extends RandomizedTest {
   @Test
@@ -50,5 +58,17 @@ public class Test003UsingRandomness extends RandomizedTest {
     int b = randomIntBetween(0, Integer.MAX_VALUE);
     int result = Adder.add(a, b);
     assertTrue(result + " < (" + a + " or " + b + ")?", result >= a && result >= b);
+  }
+
+  @Test
+  public void expectNoException() {
+    String [] words = {"oh", "my", "this", "is", "bad."};
+    
+    // This will pick a random word from the array above...
+    System.out.println(words[Math.abs(randomInt()) % words.length]);
+    
+    // .. unless the random picked happens to be this number (fixed to get 
+    // a reproducible example, but you get the idea);
+    System.out.println(words[Math.abs(Integer.MIN_VALUE) % words.length]);
   }
 }
---
+++ examples/src/test/java/com/carrotsearch/examples/randomizedrunner/Test004MoreRandomness.java
@@ -0,0 +1,85 @@
+package com.carrotsearch.examples.randomizedrunner;
+
+import java.util.Random;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.carrotsearch.randomizedtesting.RandomizedContext;
+import com.carrotsearch.randomizedtesting.RandomizedRunner;
+import com.carrotsearch.randomizedtesting.RandomizedTest;
+import com.carrotsearch.randomizedtesting.annotations.Seed;
+import com.carrotsearch.randomizedtesting.annotations.Seeds;
+
+/**
+ * Randomness is entwined everywhere in {@link RandomizedRunner}. An instance of
+ * {@link Random} is of course available from a {@link RandomizedContext}, but
+ * even tests themselves are randomized, or to be more precise: their order of
+ * execution is shuffled.
+ * 
+ * <p>
+ * In this example we have two classes that contain three methods (they're bound
+ * under a single suite class for simplicity). Every execution of class
+ * {@link Test004MoreRandomness.TestsOrderRandomized} will be different, shuffling
+ * methods order around (and the random numbers written to the output). 
+ * We can "fix" the execution order by forcing the master random
+ * seed using {@link Seed} annotation on the class (or a system property
+ * {@link RandomizedRunner#SYSPROP_RANDOM_SEED}). Doing so also fixes all derivative random
+ * generators in all tests so every execution of 
+ * {@link Test004MoreRandomness.TestsOrderRandomizedButFixed}
+ * class will be identical (and will emit identical pseudo-random numbers).
+ * 
+ * <p>
+ * All this is meant for one purpose: help in reproducing a failed randomized test
+ * case. Once a test case fails, make sure you write down the random seed number
+ * that caused the failure and add appropriate {@link Seeds} annotation on the method
+ * that failed like so:
+ * <pre>
+ * @Seeds({
+ *   @Seed("012345"),
+ *   @Seed()
+ * })
+ * </pre>
+ * where 012345 is the replaced by the seed that caused the failure. This makes
+ * the test methods run with a fixed seed once and then with a random seed again,
+ * easily creating a new regression test so that the bug does not reoccur in the 
+ * future. An example of that is shown in 
+ * {@link Test004MoreRandomness.TestsOrderRegression#regression()}. Also note
+ * how {@link RandomizedRunner} names test cases for such "expanded" methods. This
+ * is needed to avoid duplicate test {@link Description} objects (a design 
+ * flaw in JUnit).
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+  Test004MoreRandomness.TestsOrderRandomized.class,
+  Test004MoreRandomness.TestsOrderRandomizedButFixed.class,
+  Test004MoreRandomness.TestsOrderRegression.class
+})
+public class Test004MoreRandomness {
+  public static class TestsOrderRandomized extends RandomizedTest {
+    @Test public void method1() { System.out.println("method1, " + randomInt()); }
+    @Test public void method2() { System.out.println("method2, " + randomInt()); }
+    @Test public void method3() { System.out.println("method3, " + randomInt()); }
+
+    @AfterClass 
+    public static void empty() {
+      System.out.println("--");
+    }
+  }
+
+  @Seed("deadbeef")
+  public static class TestsOrderRandomizedButFixed extends TestsOrderRandomized {
+  }
+
+  public static class TestsOrderRegression extends RandomizedTest {
+    @Seeds({
+      @Seed("deadbeef"),
+      @Seed()
+    })
+    @Test public void regression() { System.out.println("regression, " + randomInt()); }
+  }  
+}
