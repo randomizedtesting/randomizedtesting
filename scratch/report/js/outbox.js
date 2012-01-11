6c5af4f7c4adc114fce700488c3a9a0dba37299b
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-01-11 12:41:48 +0100

    Updated outbox to show suite errors.

--- scratch/report/js/outbox.js
+++ scratch/report/js/outbox.js
@@ -7,18 +7,70 @@
   var data;
 
   // This function will be called by the JSONP data file
-  window.testData = function(d) {
+    window.testData = function(d) {
     data = d.suites;
+
+    var descriptionsById = { };
+
+    // Index descriptions by id
+    for (var i = 0; i < data.length; i++) {
+      map(data[i].description.children);
+    }
+
+    // Link object descriptions, split method names into semantic parts
+    eachTest(data, function (test) {
+      var description = descriptionsById[test.description];
+      test.description = description;
+
+      description.packageClassName = description.className;
+
+      var methodSplit = description.methodName.split(" ");
+      description.methodName = methodSplit[0];
+      description.methodExtras = methodSplit[1];
+
+      description.packageClassMethodName = description.className + "." + description.methodName;
+
+      var classSplit = description.className.split("\.");
+      description.className = classSplit.pop();
+      description.packageName = classSplit.join(".");
+    });
+    
+    // Link descriptions in the event list.
+    $.each(data, function(i, suite) {
+      $.each(suite.executionEvents, function(j, evt) {
+        if ("description" in evt) {
+          evt.description = descriptionsById[evt.description];
+        }
+      });
+    });
+
+    function map(children) {
+      for (var j = 0; j < children.length; j++) {
+        var child = children[j];
+        if (typeof child == 'object') {
+          descriptionsById[children[j].id] = child;
+          if (child.children) {
+            map(child.children);
+          }
+        } else {
+          children[j] = descriptionsById[child];
+        }
+      }
+    }
+    
+    function eachTest(data, callback) {
+      for (var i = 0; i < data.length; i++) {
+        var tests = data[i].tests;
+        for (var j = 0; j < tests.length; j++) {
+          callback.call(this, tests[j]);
+        }
+      }
+    }    
   }
 
   Array.prototype.peek = function() {
     return this[this.length - 1];
   };
-
-  function testName(testEvent) {
-    var name = testEvent.test;
-    return new RegExp("^[^\\ \\(]*").exec(name);
-  }
 
   function redrawConnectors() {
     var $content = $("#content");
@@ -65,6 +117,20 @@
 
       $.each(suite.executionEvents, function(index, evtobj) {
         switch (evtobj.event) {
+          case "SUITE_FAILURE":
+            // Add a marker.
+            var failureMarker = $("<span class='failure marker' />");
+            failureMarker.appendTo(stack.peek());
+            
+            var label = $("<span class='suitefailure'>suite failure</span>");
+            stack.peek().append(
+              $("<span class='side' />").append(
+                $("<div />").append(
+                  label)));
+
+            failureMarker.data("refLabel", label);
+            break;
+
           case "TEST_STARTED":
             // Add a content wrapper for the test...
             var testArea = $("<span class='test' />").appendTo(stack.peek());
@@ -75,7 +141,7 @@
             startMarker.appendTo(testArea);
 
             // ...and a side label.
-            var label = $("<span class='label'>" + testName(evtobj) + "</span>");
+            var label = $("<span class='test label'>" + evtobj.description.methodName + "</span>");
             testArea.append(
               $("<span class='side' />").append(
                 $("<div />").append(
