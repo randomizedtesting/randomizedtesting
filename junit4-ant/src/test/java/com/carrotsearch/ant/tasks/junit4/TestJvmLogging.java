a341411f595f7cee63a29a8892261ba125df965f
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-04-20 09:45:01 +0200

    A test case that fails with -verbose.

---
+++ junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/TestJvmLogging.java
@@ -0,0 +1,25 @@
+package com.carrotsearch.ant.tasks.junit4;
+
+import java.io.File;
+import java.net.URL;
+
+import org.junit.*;
+
+/**
+ * Check JVM logging settings. Thet seem to use process descriptors not
+ * {@link System} streams.
+ */
+@Ignore("https://github.com/carrotsearch/randomizedtesting/issues/87")
+public class TestJvmLogging extends AntBuildFileTestBase {
+  
+  @Before
+  public void setUp() throws Exception {
+    URL resource = getClass().getClassLoader().getResource("junit4.xml");
+    super.setupProject(new File(resource.getFile()));
+  }
+  
+  @Test
+  public void jvmverbose() {
+    executeTarget("jvmverbose");
+  }
+}
