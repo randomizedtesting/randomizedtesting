ab7ab31a89c92b61609c58769ebbc295f8b9bdeb
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-07-02 09:35:20 +0200

    Splitting TestJUnit4 into smaller suites.

---
+++ junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/TestSysProperties.java
@@ -0,0 +1,49 @@
+package com.carrotsearch.ant.tasks.junit4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSysProperties extends JUnit4XmlTestBase {
+  @Test 
+  public void failureProperty() {
+    executeTarget("failureProperty");
+  }
+
+  @Test
+  public void failureTypePassing() {
+    executeTarget("failureTypePassing");
+    assertLogContains("Throwable #1: com.carrotsearch.ant.tasks.junit4.tests.SyntheticException");
+    assertLogContains("Tests summary: 1 suite, 1 test, 1 error");
+  }
+
+  @Test
+  public void escaping() {
+    executeTarget("escaping");
+    assertLogContains("\"-Dsysprop.key2=abc def\"");
+  }
+
+  @Test 
+  public void sysproperty() {
+    executeTarget("sysproperty");
+  }
+
+  @Test 
+  public void env() {
+    executeTarget("env");
+  }
+
+  @Test
+  public void iters() {
+    executeTarget("iters");
+    Pattern p = Pattern.compile("TestSuccess\\.alwaysPasses");
+    Matcher matcher = p.matcher(getLog());
+    int count = 0;
+    while (matcher.find()) {
+      count++;
+    }
+    Assert.assertEquals(5, count);
+  }
+}
