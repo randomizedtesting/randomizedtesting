ca1323d3308b7fd3709b63f297fbc46286a18a6e
Dawid Weiss <dawid.weiss@carrotsearch.com>
2012-07-30 12:08:11 +0200

    J9 with dumps.

--- randomized-runner/src/test/java/com/carrotsearch/randomizedtesting/TestFormattingRandomSeeds.java
+++ randomized-runner/src/test/java/com/carrotsearch/randomizedtesting/TestFormattingRandomSeeds.java
@@ -1,6 +1,10 @@
 package com.carrotsearch.randomizedtesting;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -23,10 +27,43 @@ public class TestFormattingRandomSeeds {
     check(Long.MAX_VALUE);
   }
 
+  volatile static int progress = 0;
+
+  @BeforeClass
+  public static void beforeClass() {
+    new Thread("dumper") {
+      {
+        setDaemon(true);
+      }
+
+      public void run() {
+        try {
+          while (true) {
+            int p = progress;
+            Thread.sleep(1000);
+            if (p == progress) {
+              Class<?> clazz = Class.forName("com.ibm.jvm.Dump");
+              //Method m = clazz.getMethod("JavaDump");
+              Method m = clazz.getMethod("SystemDump");
+              m.invoke(null);
+
+              System.out.println("Stopped.");
+              Thread.getAllStackTraces();
+              System.out.println("Or not.");
+            }
+          }
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+    }.start();
+  }
+  
   /** Heck, why not use ourselves here? ;) */
   @Test
-  @Repeat(iterations = 100)
-  public void noise() {
+  @Repeat(iterations = 1000)
+  public void noise() throws Exception {
+    progress++;
     check(RandomizedContext.current().getRandom().nextLong());
   }
 
