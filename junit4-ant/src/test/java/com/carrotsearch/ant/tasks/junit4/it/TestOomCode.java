ef890fb197891fc798d1ac41e7c1783bbd5d23b8
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-01-01 16:44:39 +0100

    GH-140: increase timeouts for slower machines.

--- junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/it/TestOomCode.java
+++ junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/it/TestOomCode.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestOomCode  extends JUnit4XmlTestBase {
   @Test
   public void oom() {
-    super.executeForkedTarget("oomcode");
+    super.executeForkedTarget("oomcode", 5 * 60 * 1000L);
     assertLogContains("Forked JVM ran out of memory");
     assertLogContains("WARN: JVM out of memory");
   }
