8cf4d189f576218414146c3df0ab879cffc409e5
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-10-30 11:19:37 +0100

    Adding an old mac sys threads.

---
+++ randomized-runner/src/test/java/com/carrotsearch/randomizedtesting/TestMacSysThreads.java
@@ -0,0 +1,38 @@
+package com.carrotsearch.randomizedtesting;
+
+import java.lang.management.ManagementFactory;
+
+import javax.management.MBeanServer;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakAction;
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakAction.Action;
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope.Scope;
+
+/**
+ * Test Mac system threads.
+ */
+public class TestMacSysThreads extends WithNestedTestClass {
+  @ThreadLeakScope(Scope.SUITE)
+  @ThreadLeakAction({Action.WARN})
+  public static class Nested extends RandomizedTest {
+    @Test
+    public void testMethod1() {
+      MBeanServer mb = ManagementFactory.getPlatformMBeanServer();
+      mb.getMBeanCount();
+      RandomizedTest.sleep(5000);
+    }
+  }
+
+  @Test
+  public void testSuccessful() {
+    Result result = JUnitCore.runClasses(Nested.class);
+    Assert.assertEquals(0, result.getFailureCount());
+  }
+}
