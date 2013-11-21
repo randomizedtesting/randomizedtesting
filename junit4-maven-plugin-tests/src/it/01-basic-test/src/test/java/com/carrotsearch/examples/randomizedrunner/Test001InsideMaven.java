0bfa9e8c6da0bdf2a8defb538be96b35a60886fb
Dawid Weiss <dawid.weiss@carrotsearch.com>
2013-11-22 00:43:38 +0100

    Fixes GH-153: maven plugin should support argsLine much like surefire.

--- junit4-maven-plugin-tests/src/it/01-basic-test/src/test/java/com/carrotsearch/examples/randomizedrunner/Test001InsideMaven.java
+++ junit4-maven-plugin-tests/src/it/01-basic-test/src/test/java/com/carrotsearch/examples/randomizedrunner/Test001InsideMaven.java
@@ -1,10 +1,17 @@
 package com.carrotsearch.examples.randomizedrunner;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Test001InsideMaven {
   @Test
   public void success() {
     // Empty.
+  }
+
+  @Test
+  public void checkArgLinePassed() {
+      Assert.assertEquals("foobar", System.getProperty("argLine.property"));
+      Assert.assertEquals("foobar", System.getProperty("argLine.property2"));
   }
 }
