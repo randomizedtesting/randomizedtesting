5eaa7b102b2ea3ef5d12ee746cbb2ddbdc396533
Dawid Weiss <dawid.weiss@carrotsearch.com>
2014-02-27 11:18:41 +0100

    Adding missing files.

---
+++ junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/tests/replication/TestNonReplicated1.java
@@ -0,0 +1,12 @@
+package com.carrotsearch.ant.tasks.junit4.tests.replication;
+
+import org.junit.Test;
+
+import com.carrotsearch.randomizedtesting.RandomizedTest;
+
+public class TestNonReplicated1 extends RandomizedTest {
+  @Test
+  public void nonReplicatedTest() {
+    System.out.println("Non-replicated test, VM: " + System.getProperty("junit4.childvm.id"));
+  }
+}
