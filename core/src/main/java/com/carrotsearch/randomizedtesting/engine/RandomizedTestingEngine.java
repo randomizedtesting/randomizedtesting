ee535bb79495cb9c8eb3f0b6db2ba95de6a3039a
Dawid Weiss <dawid.weiss@carrotsearch.com>
2021-08-31 14:53:12 +0200

    Raw TestEngine.

--- core/src/main/java/com/carrotsearch/randomizedtesting/engine/RandomizedTestingEngine.java
+++ core/src/main/java/com/carrotsearch/randomizedtesting/engine/RandomizedTestingEngine.java
@@ -1,51 +1,51 @@
 package com.carrotsearch.randomizedtesting.engine;
 
 import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.EngineExecutionListener;
 import org.junit.platform.engine.ExecutionRequest;
 import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.TestEngine;
+import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.UniqueId;
 import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor;
-import org.junit.platform.engine.support.discovery.EngineDiscoveryRequestResolver;
 import org.junit.platform.engine.support.hierarchical.EngineExecutionContext;
-import org.junit.platform.engine.support.hierarchical.HierarchicalTestEngine;
 import org.junit.platform.engine.support.hierarchical.Node;
 
-public class RandomizedTestingEngine
-    extends HierarchicalTestEngine<RandomizedTestingEngine.RandomizedContext> {
-
-  private final RandomizedTestDescriptor engineDescriptor;
-
-  public RandomizedTestingEngine() {
-    this.engineDescriptor =
-        new RandomizedTestDescriptor(
-            UniqueId.forEngine(getId()), getId(), TestDescriptor.Type.CONTAINER);
-  }
-
-  @Override
-  protected RandomizedContext createExecutionContext(ExecutionRequest executionRequest) {
-    return new RandomizedContext();
-  }
+public class RandomizedTestingEngine implements TestEngine {
+  public RandomizedTestingEngine() {}
 
   @Override
   public String getId() {
     return "com.carrotsearch.randomizedtesting";
   }
 
-  private static final EngineDiscoveryRequestResolver<TestDescriptor> resolver =
-      EngineDiscoveryRequestResolver.builder()
-          .addClassContainerSelectorResolver(
-              clazz -> {
-                System.out.println("class resolver: " + clazz.getName());
-                return true;
-              })
-          .build();
-
   @Override
   public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
+    var engineDescriptor =
+        new RandomizedTestDescriptor(
+            UniqueId.forEngine(getId()), getId(), TestDescriptor.Type.CONTAINER);
+
     engineDescriptor.addChild(
         new RandomizedTestDescriptor(
             uniqueId.append("seg", "test-foo"), "foo", TestDescriptor.Type.TEST));
+
     return engineDescriptor;
+  }
+
+  @Override
+  public void execute(ExecutionRequest request) {
+    var listener = request.getEngineExecutionListener();
+    process(listener, request.getRootTestDescriptor());
+  }
+
+  private void process(EngineExecutionListener listener, TestDescriptor node) {
+    listener.executionStarted(node);
+    if (node.isContainer()) {
+      for (var v : node.getChildren()) {
+        process(listener, v);
+      }
+    }
+    listener.executionFinished(node, TestExecutionResult.successful());
   }
 
   public static class RandomizedContext implements EngineExecutionContext {}
