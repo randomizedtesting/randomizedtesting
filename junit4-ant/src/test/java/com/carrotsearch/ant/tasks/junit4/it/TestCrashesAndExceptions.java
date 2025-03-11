4f304aedfbe90c0a57027d28df9cbea71498e91b
Dawid Weiss <dawid.weiss@carrotsearch.com>
2025-03-11 16:22:10 +0100

    Ignore the test if crashlib cannot be linked.

--- junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/it/TestCrashesAndExceptions.java
+++ junit4-ant/src/test/java/com/carrotsearch/ant/tasks/junit4/it/TestCrashesAndExceptions.java
@@ -3,7 +3,12 @@ package com.carrotsearch.ant.tasks.junit4.it;
 
 import java.io.File;
 
+import org.apache.tools.ant.BuildException;
+import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
 
 
 public class TestCrashesAndExceptions extends JUnit4XmlTestBase {
@@ -16,7 +21,18 @@ public class TestCrashesAndExceptions extends JUnit4XmlTestBase {
 
   @Test
   public void jvmcrash() {
-    expectBuildExceptionContaining("jvmcrash", "was not empty, see:");
+    try {
+      executeTarget("jvmcrash");
+      Assert.fail("Expected a build failure.");
+    } catch (BuildException e) {
+      String log = getLog();
+      if (log.contains("java.lang.UnsatisfiedLinkError: Could not link with crashlib")) {
+        // ignore
+        Assume.assumeTrue(false);
+      }
+      Assert.assertThat(e.getMessage(), containsString("was not empty, see:"));
+    }
+
     File cwd = getProject().getBaseDir();
     for (File crashDump : cwd.listFiles()) {
       if (crashDump.isFile() && 
